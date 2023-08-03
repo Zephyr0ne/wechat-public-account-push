0ec85cb6a4f79f96563018163cd9ef275ab15494
yyl <990258262@qq.com>
2023-08-03 15:07:13 +0800

    add

--- config/index.cjs
+++ config/index.cjs
@@ -2,62 +2,104 @@
 
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  //推送通道
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
+  APP_ID: 'wx5047b8b6c2c4e016',
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'c14dfb7131ac59ae5c154bf502a66385',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '四川',
+  CITY: '成都',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      name: 'zephyr0ne',
+      // PushDeer的key
+      id: 'PDU18514TlSrOLZmqKVQHwOnZ39ObhuQ9PeZUeH2a',
+      // template-config中的id
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-28',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        {type: '*生日', name: '您', year: '1998', date: '06-06',isShowAge: true,},
+        {type: '节日', name: '你和你老婆的初见纪念日', year: '2021', date: '07-15',},
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2021-07-28' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-09-29' },
       ],
     },
+    // {
+    //   // 想要发送的人的名字
+    //   name: '大宝贝',
+    //   // PushDeer的key
+    //   id: 'PDU18519T7b5aS7GRnzBWsejVj8bsEqnfnDAqA9Nf',
+    //   // template-config中的id
+    //   useTemplateId: '0002',
+    //   // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+    //   horoscopeDate: '01-30',
+    //   festivals: [
+    //     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    //     {type: '*生日', name: '您', year: '1998', date: '01-03',isShowAge: true,},
+    //     {type: '节日', name: '你和你老公的初见纪念日', year: '2021', date: '07-15',},
+    //   ],
+    //   // 我们在一起已经有xxxx天了的配置
+    //   customizedDateList: [
+    //     // 在一起的日子
+    //     { keyword: 'love_day', date: '2021-07-28' },
+    //     // 结婚纪念日
+    //     { keyword: 'marry_day', date: '2022-09-29' },
+    //   ],
+    // },
+
+    //更多人员配置依次往下
+    // {
+    //   // 想要发送的人的名字
+    //   name: '大帅哥',
+    //   // PushDeer的key
+    //   id: 'PDU18514TnkkUuF2kj3wbBOiiJd2xTGkM0VN4XXia',
+    //   // template-config中的id
+    //   useTemplateId: '0001',
+    //   // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+    //   horoscopeDate: '07-28',
+    //   festivals: [
+    //     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    //     {type: '*生日', name: '您', year: '1998', date: '06-06',isShowAge: true,},
+    //     {type: '节日', name: '你和欣欣大宝贝儿的搭讪纪念日', year: '2021', date: '07-15',},
+    //   ],
+    //   // 我们在一起已经有xxxx天了的配置
+    //   customizedDateList: [
+    //     // 在一起的日子
+    //     { keyword: 'love_day', date: '2021-07-28' },
+    //     // 结婚纪念日
+    //     { keyword: 'marry_day', date: '2022-09-29' },
+    //   ],
+    // },
+
+
+
+
   ],
 
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  // template-config.js中的id
+  CALLBACK_TEMPLATE_ID: '9999',
 
   CALLBACK_USERS: [
     {
       name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      // pushDeer中的key
+      // id: 'PDU18514TnkkUuF2kj3wbBOiiJd2xTGkM0VN4XXia',
     }
   ],
 
--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,41 +2,102 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
-    desc: `
+    title: '又是美好的一天',
+    desc:
+    /*开始*/
+      `
       **{{date.DATA}}**
       
+    ---      
       下个休息日：{{holidaytts.DATA}}
-      
-      ---
+    
+    ---
       
       城市：{{city.DATA}}
       
       天气：{{weather.DATA}}
       
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      最低气温: {{min_temperature.DATA}}
+      
+      最高气温：{{max_temperature.DATA}}
       
       风向: {{wind_direction.DATA}}
       
       风级: {{wind_scale.DATA}}
       
-      {{comprehensive_horoscope.DATA}}
+      提醒:  {{ganmao.DATA}}
       
+      注意:  {{notice.DATA}}
+    
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是你和你老婆相识的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
       ---
       
-      {{moment_copyrighting.DATA}}
+      每日一言： {{one_talk.DATA}}
       
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+      土味情话： {{earthy_love_words.DATA}}
+
+     
+      {{comprehensive_horoscope.DATA}}
+
+
     `
+    /*结束*/
   },
   {
     id: '0002',
+    title: '一日之计在于晨',
+    desc:
+    /*开始*/
+      `
+      **{{date.DATA}}**
+      
+    ---      
+      下个休息日：{{holidaytts.DATA}}
+    
+    ---
+      
+      城市：{{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      最低气温: {{min_temperature.DATA}}
+      
+      最高气温：{{max_temperature.DATA}}
+      
+      风向: {{wind_direction.DATA}}
+      
+      风级: {{wind_scale.DATA}}
+      
+      提醒:  {{ganmao.DATA}}
+      
+      注意:  {{notice.DATA}}
+    
+      ---
+      
+      今天是你和你老公相识的第{{love_day.DATA}}天
+      
+      {{birthday_message.DATA}}
+      
+      ---
+      
+      每日一言： {{one_talk.DATA}}
+      
+      土味情话： {{earthy_love_words.DATA}}
+
+     
+      {{comprehensive_horoscope.DATA}}
+
+
+    `
+    /*结束*/
+  },
+  {
+    id: '9999',
     title: '推送完成提醒',
     desc: `
       服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
@@ -52,6 +113,7 @@ const TEMPLATE_CONFIG = [
       失败用户: {{fail_post_ids.DATA}}
     `
   },
+
 ]
 
 module.exports = TEMPLATE_CONFIG
